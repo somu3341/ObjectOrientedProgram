eff1322a55241907449632d8a345926b05c8078c
somu3341 <somashekarvsv1996@gmail.com>
2023-03-16 16:20:30 +0530

    [somu]Add:Json inventory data management of Rice, pulses, Wheat.

---
+++ ObjectOrientedProgram/InventoryDataManagementProblems/InventoryOperation.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgram.InventoryDataManagementProblems
+{
+   public class InventoryOperation
+    {
+        public void ReadJsonfile(string filepath)
+        {
+            var data=File.ReadAllText(filepath);
+            var result = JsonConvert.DeserializeObject<List<InventoryData>>(data);
+            foreach (var inventory in result)
+            {
+                Console.WriteLine(inventory.Name + "\t" + inventory.Weight + "\t" + inventory.PricePerKg +
+                      "\t" + inventory.Weight * inventory.PricePerKg);
+            }
+        }
+    }
+}
---
+++ ObjectOrientedProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using ObjectOrientedProgram.InventoryDataManagementProblems;
+using System;
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag) 
+            {
+                Console.WriteLine("Select option to perform \n1.Json Inventory \n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryOperation inventory = new InventoryOperation();
+                        inventory.ReadJsonfile(@"D:\BridgeLabs\ObjectOrientedProgram\ObjectOrientedProgram\InventoryDataManagementProblems\Inventory.json");
+                        break;
+                   case 2:flag = false;
+                        break;
+                }
+            }
+            
+            
+        }
+    }
+}
